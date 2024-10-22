ffa029657307c95f6b7c09875ebb950327566151
rayanarocha <rayana.azus@gmail.com>
2024-10-22 15:32:55 -0300

    desafio data

---
+++ testes/desafiodata.py
@@ -0,0 +1,25 @@
+from datetime import date, datetime, time
+
+
+
+data = date(2024, 10, 22)
+
+print(data)
+
+print(date.today())
+
+
+
+
+
+data_hora = datetime(2024, 10, 22)
+
+print(data_hora)
+
+print(datetime.today())
+
+
+
+hora = time(15, 28, 0)
+
+print(hora)
